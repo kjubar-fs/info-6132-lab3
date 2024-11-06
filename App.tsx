595a78702397caf1b379a0e07131b6c4d6ec16b4
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-06 10:45:26 -0500

    Add nav theme

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 6 Nov 2024, 9:40:17 AM
- *  Last update: 6 Nov 2024, 10:38:27 AM
+ *  Last update: 6 Nov 2024, 10:45:07 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { StatusBar } from 'expo-status-bar';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer, Theme } from '@react-navigation/native';
 
 import { AppScreen } from './src/screens/AppScreen';
+import { primaryColor } from './src/util/constants';
+
+const BooksTheme: Theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: primaryColor,
+    },
+}
 
 export default function App() {
     return (
-        <NavigationContainer>
+        <NavigationContainer theme={BooksTheme}>
             <StatusBar style="light" />
 
             <AppScreen />
