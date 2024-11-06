f984970e064ec72ea267afd40c99215f47d4de91
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-06 10:55:25 -0500

    Style tab bar

--- src/screens/AppScreen/index.tsx
+++ src/screens/AppScreen/index.tsx
@@ -1,10 +1,13 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 6 Nov 2024, 9:47:58 AM
- *  Last update: 6 Nov 2024, 10:35:47 AM
+ *  Last update: 6 Nov 2024, 10:52:20 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 import { HomeScreen } from "../HomeScreen";
 import { BorrowedScreen } from "../BorrowedScreen";
@@ -16,16 +19,30 @@ export function AppScreen(): JSX.Element {
         <Tab.Navigator
             screenOptions={{
                 headerShown: false,
+                tabBarStyle: {
+                    height: 60,
+                    paddingBottom: 8,
+                },
             }}
         >
             <Tab.Screen
                 name="Home"
                 component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ color, size }): JSX.Element => (
+                        <Ionicons name="library" size={size} color={color} />
+                    ),
+                }}
             />
             
             <Tab.Screen
                 name="Borrowed"
                 component={BorrowedScreen}
+                options={{
+                    tabBarIcon: ({ color, size }): JSX.Element => (
+                        <MaterialIcons name="library-books" size={size} color={color} />
+                    ),
+                }}
             />
         </Tab.Navigator>
     );
