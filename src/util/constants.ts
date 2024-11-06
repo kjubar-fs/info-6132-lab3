de161bc879f928c332ac495c2c09515e00bf3aa2
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-06 10:40:30 -0500

    Add colors

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,9 +1,12 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 6 Nov 2024, 10:22:29 AM
- *  Last update: 
+ *  Last update: 6 Nov 2024, 10:40:10 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { Platform } from "react-native";
 
 export const safeAreaPadding = Platform.OS === "ios" ? 60 : 50;
+
+export const primaryColor = "#948033";
+export const accentColor = "#DCCE80";
