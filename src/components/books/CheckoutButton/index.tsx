9dac45751418d481943bcd8abb756a946c53c9fa
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-07 13:47:52 -0500

    Disable check out button when at max books

--- src/components/books/CheckoutButton/index.tsx
+++ src/components/books/CheckoutButton/index.tsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 7 Nov 2024, 12:43:12 PM
- *  Last update: 7 Nov 2024, 12:48:20 PM
+ *  Last update: 7 Nov 2024, 1:46:35 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { TouchableHighlight, Text, ViewStyle } from "react-native";
@@ -11,6 +11,12 @@ import { toggleBookStatus, useBooks } from "../../../data/state/books";
 
 import styles from "./styles";
 
+type ButtonConfig = {
+    backgroundColor: string,
+    underlayColor: string,
+    buttonText: string,
+};
+
 interface Props {
     book: Book,
     style?: ViewStyle,
@@ -18,18 +24,46 @@ interface Props {
 
 export function CheckoutButton({ book, style }: Props): JSX.Element {
     const booksState = useBooks();
+    const maxBooks = booksState.numCheckedOut >= 3;
+
+    
+    let action: (() => void) | undefined = () => {
+        toggleBookStatus(book.id, booksState);
+    };
+    let buttonConfig: ButtonConfig = {
+        backgroundColor: "#7E7",
+        underlayColor: "#4C4",
+        buttonText: "Check Out",
+    };
+
+    if (!book.checkedOut && maxBooks) {
+        // no onPress action if we're at the max books already, effectively disabling the button
+        action = undefined;
+        // also set disabled config
+        buttonConfig = {
+            backgroundColor: "#CCC",
+            underlayColor: "#CCC",
+            buttonText: "Max Checked Out",
+        };
+    } else if (book.checkedOut) {
+        buttonConfig = {
+            backgroundColor: "#E77",
+            underlayColor: "#C44",
+            buttonText: "Return",
+        };
+    }
 
     return (
         <TouchableHighlight
             style={[
                 styles.checkOut,
-                { backgroundColor: book.checkedOut ? "#EE7777" : "#77EE77" },
+                { backgroundColor: buttonConfig.backgroundColor },
                 style,
             ]}
-            onPress={() => { toggleBookStatus(book.id, booksState) }}
-            underlayColor={book.checkedOut ? "#CC4444" : "#44CC44"}
+            onPress={action}
+            underlayColor={buttonConfig.underlayColor}
         >
-            <Text style={styles.checkOutText}>{book.checkedOut ? "Return" : "Check Out"}</Text>
+            <Text style={styles.checkOutText}>{buttonConfig.buttonText}</Text>
         </TouchableHighlight>
     );
 }
