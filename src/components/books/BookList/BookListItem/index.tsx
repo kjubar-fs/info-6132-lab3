3f49cac3ead740cb2557db67d3e26aa09cff63e6
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-07 00:09:08 -0500

    Reorder imports

--- src/components/books/BookList/BookListItem/index.tsx
+++ src/components/books/BookList/BookListItem/index.tsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 Oct 2024, 1:49:51 PM
- *  Last update: 7 Nov 2024, 12:08:02 AM
+ *  Last update: 7 Nov 2024, 12:09:03 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { View, Text, TouchableHighlight } from "react-native";
@@ -13,8 +13,9 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 import { Book } from "../../../../data/firebase/config";
 
-import styles from "./styles";
 import { HomeStackParamList } from "../../../../screens/HomeScreen";
+
+import styles from "./styles";
 
 interface Props {
     book: Book,
