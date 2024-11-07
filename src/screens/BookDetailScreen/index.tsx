da890b1f39238b7d3f9be322eb6082a2300f8a10
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-07 09:22:09 -0500

    Add description and newline parsing

--- src/screens/BookDetailScreen/index.tsx
+++ src/screens/BookDetailScreen/index.tsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 6 Nov 2024, 11:00:26 AM
- *  Last update: 7 Nov 2024, 12:11:45 AM
+ *  Last update: 7 Nov 2024, 9:19:45 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { Text } from "react-native";
@@ -14,7 +14,11 @@ import { HomeStackParamList } from "../HomeScreen";
 type Props = NativeStackScreenProps<HomeStackParamList, "bookDetail">;
 
 export function BookDetailScreen({ route }: Props): JSX.Element {
+    const book = route.params.book;
     return (
-        <Text>Book details for book {route.params.book.id} here</Text>
+        <>
+            <Text>Book details for book {book.id} here</Text>
+            <Text>{book.description.replaceAll("\\n", "\n")}</Text>
+        </>
     );
 }
