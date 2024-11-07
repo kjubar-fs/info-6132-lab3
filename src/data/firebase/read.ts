eb0dc9f2ba7abfc93e1d0de337b5831488fdf5f4
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-07 11:37:37 -0500

    Tweak error logging, add comments

--- src/data/firebase/read.ts
+++ src/data/firebase/read.ts
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 6 Nov 2024, 11:47:44 PM
- *  Last update: 7 Nov 2024, 12:10:46 AM
+ *  Last update: 7 Nov 2024, 11:37:25 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { getDocs, query, collection, orderBy } from "firebase/firestore";
@@ -9,12 +9,13 @@ import { getDocs, query, collection, orderBy } from "firebase/firestore";
 import { db, Book, booksCollection } from "./config";
 
 /**
- * @returns all books in the database, sorted by title
+ * @returns all books in the database, sorted by title, or undefined if loading failed
  */
 export async function loadAllBooks(): Promise<Book[] | undefined> {
     let books: Book[] = [];
 
     try {
+        // get a snapshot of all books in the database, sorted by title
         const snapshot = await getDocs(
             query(
                 collection(db, booksCollection),
@@ -22,14 +23,17 @@ export async function loadAllBooks(): Promise<Book[] | undefined> {
             )
         );
 
+        // cast docs as Books and add to list
         snapshot.forEach((bookDoc) => {
             const newBook = bookDoc.data() as Book;
             newBook.id = bookDoc.id;
             books.push(newBook);
         });
-    } catch (e) {
-        console.error(e);
+    } catch (err) {
+        // caught an error, log it and return undefined to indicate error
+        console.error(`Error getting books:`, err);
+        return undefined;
     }
 
-    return books.length !== 0 ? books : undefined;
+    return books;
 }
