f3ea1d1d918b14b40584800af5c632074e57aa92
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-06 23:23:53 -0500

    Add drop shadow styles

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 6 Nov 2024, 10:22:29 AM
- *  Last update: 6 Nov 2024, 10:40:10 AM
+ *  Last update: 6 Nov 2024, 11:22:59 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { Platform } from "react-native";
@@ -10,3 +10,27 @@ export const safeAreaPadding = Platform.OS === "ios" ? 60 : 50;
 
 export const primaryColor = "#948033";
 export const accentColor = "#DCCE80";
+
+export const dropShadowStyle = {
+    // remove shadowColor on Android because it conflicts with elevation
+    shadowColor: Platform.OS === "ios" ? "#666" : undefined,
+    shadowOffset: {
+        width: 0,
+        height: 3,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 3,
+};
+
+export const lightDropShadowStyle = {
+    // remove shadowColor on Android because it conflicts with elevation
+    shadowColor: Platform.OS === "ios" ? "#999" : undefined,
+    shadowOffset: {
+        width: 0,
+        height: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2,
+};
