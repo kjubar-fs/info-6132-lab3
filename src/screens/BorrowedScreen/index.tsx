796bcfb241b411820accd123232c5d43ca318576
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-07 00:34:44 -0500

    Add borrowed books list

--- src/screens/BorrowedScreen/index.tsx
+++ src/screens/BorrowedScreen/index.tsx
@@ -1,17 +1,19 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 6 Nov 2024, 10:35:06 AM
- *  Last update: 
+ *  Last update: 7 Nov 2024, 12:33:56 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { Text } from "react-native";
 
 import { ScreenWrapper } from "../../components/common/ScreenWrapper";
+import { BookList } from "../../components/books/BookList";
 
 export function BorrowedScreen(): JSX.Element {
     return (
-        <ScreenWrapper contentContainerStyle={{ justifyContent: "center", alignItems: "center" }}>
+        <ScreenWrapper>
             <Text>Borrowed</Text>
+            <BookList borrowedOnly={true} />
         </ScreenWrapper>
     );
 }
