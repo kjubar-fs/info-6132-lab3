78086e3c03a0394e0537cae1264b8309e8514c40
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-07 00:15:32 -0500

    Remove unused Props interface

--- src/components/books/BookList/index.tsx
+++ src/components/books/BookList/index.tsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 Oct 2024, 1:48:36 PM
- *  Last update: 7 Nov 2024, 12:11:11 AM
+ *  Last update: 7 Nov 2024, 12:15:26 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { View, FlatList } from "react-native";
@@ -11,10 +11,6 @@ import { useBooks } from "../../../data/state/books";
 import { BookListItem } from "./BookListItem";
 
 import styles from "./styles";
-
-interface Props {
-
-}
 
 export function BookList() {
     const books = useBooks().books;
